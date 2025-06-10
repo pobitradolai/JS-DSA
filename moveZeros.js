e39c9549b49738cf28e40f7e3bc23d01c571802c
Pobitra Dolai <92564154+pobitradolai@users.noreply.github.com>
2025-06-10 12:00:33 +0530

    Create moveZeros.js

---
+++ moveZeros.js
@@ -0,0 +1,15 @@
+function moveZeros(arr) {
+  let insertPos = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] !== 0 ){
+      arr[insertPos++] = arr[i];
+    }
+    
+  }
+  while (insertPos < arr.length) {
+    arr[insertPos++] = 0;
+  }
+  return arr;
+}
+
+console.log(moveZeros([0,0, 1, 0, 3, 12])); // [ 1, 3, 12, 0, 0, 0 ]
